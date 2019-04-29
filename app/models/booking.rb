54ffb9079406333ef6c04c6105a79221dbf66813
Leon <leonboutrand@gmail.com>
2019-04-29 19:21:52 +0200

    methods aftercreate on models Reservation and Booking

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,20 @@
 class Booking < ApplicationRecord
+  after_create :create_missions
+
   belongs_to :listing
+
+  private
+
+  def create_missions
+    Mission.create(
+      mission_type: "first_checkin",
+      date: start_date,
+      price: listing.num_rooms * 10
+    )
+    Mission.create(
+      mission_type: "last_checkout",
+      date: end_date,
+      price: listing.num_rooms * 5
+    )
+  end
 end
--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -4,6 +4,17 @@ class Reservation < ApplicationRecord
   belongs_to :listing
 
   def last_checkout_exists?
+    # is not private because used in MissionGenerator
     listing.bookings.map(&:end_date).include?(end_date)
+  end
+
+  private
+
+  def create_missions
+    Mission.create(
+      mission_type: "checkin_checkout",
+      date: end_date,
+      price: listing.num_rooms * 10
+    ) unless last_checkout_exists?
   end
 end
